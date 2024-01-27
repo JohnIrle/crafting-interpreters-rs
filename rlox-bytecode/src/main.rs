648a51cf10d34f1f3f7ebc2e8f8bab60b274371d
John Irle <irlejohn@gmail.com>
2024-01-27 17:03:11 -0600

    feat: finish chapter 14

--- rlox-bytecode/src/main.rs
+++ rlox-bytecode/src/main.rs
@@ -1,3 +1,13 @@
+use rlox::chunk::Chunk;
+use rlox::chunk::OpCode::{OP_CONSTANT, OP_RETURN};
+
 fn main() {
-    println!("Hello, world");
+    let mut chunk = Chunk::new();
+
+    let constant =  chunk.add_constant(1.2);
+    chunk.write_chunk(OP_CONSTANT.value(), 123);
+    chunk.write_chunk(constant as u8, 123);
+
+    chunk.write_chunk(OP_RETURN.value(), 123);
+    chunk.disassemble_chunk("test chunk");
 }
